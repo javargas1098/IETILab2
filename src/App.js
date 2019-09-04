5a045de756671243d4374f4c5c20c9c470a5f15f
Javier Vargas <ja.vargas1098@gmail.com>
2019-09-03 20:46:23 -0500

    final

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import './App.css';
+import logo from './logo.svg';
+//import {TodoList} from './TodoList';
+import { TodoApp } from './TodoApp.js';
+import { Login } from './component/Login.js';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+
+const LoginView = () => (
+  <Login />
+);
+
+const TodoView = () => (
+  <TodoApp />
+);
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    localStorage.setItem('emailDefault', "JavierV");
+    localStorage.setItem('passwordDefault', "ieti");
+  }
+
+  render() {
+    // const todos = [{text:"Learn React", priority:5, dueDate: new Date() },
+    //       {text:"Learn about APIs", priority:4, dueDate: new Date(2018,8,30) },
+    //       {text:"write TODO App", priority:3, dueDate: new Date(2018,9,30) }];
+    return (
+      //<TodoList todoList={todos} />      
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">TODO React App</h1>
+          </header>
+
+          <br />
+          <br />
+
+          <ul>
+            {localStorage.getItem('isLoggedIn') === "true" ?
+              <li><Link to="/todo">Todo</Link></li> : <li><Link to="/">Login</Link></li>
+            }
+          </ul>
+
+          <div>
+            {localStorage.getItem('isLoggedIn') === "true" ?
+              < Route path="/todo" component={TodoView} /> : <Route exact path="/" component={LoginView} />
+            }
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
---
+++ src/component/Login.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import LockIcon from '@material-ui/icons/LockOutlined';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import './Login.css'
+
+
+export class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { email: "", password: "" };
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+    handleSubmit(e) {
+        if (localStorage.getItem('emailDefault') === this.state.email && localStorage.getItem('passwordDefault') === this.state.password)
+            localStorage.setItem('isLoggedIn', "true");
+        this.setState({ email: "", password: "" });
+    }
+
+
+    render() {
+        return (
+            <React.Fragment>
+                <CssBaseline />
+                <main className="layout">
+                    <Paper className="paper">
+                        <Avatar className="avatar">
+                            <LockIcon />
+                        </Avatar>
+                        <Typography variant="headline">Sign in</Typography>
+                        <form className="form" onSubmit={this.handleSubmit}>
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="email">Email Address</InputLabel>
+                                <Input id="email" name="email" autoComplete="email" autoFocus value={this.state.email}
+                                    onChange={event => this.setState({ email: event.target.value })} />
+                            </FormControl>
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="password">Password</InputLabel>
+                                <Input
+                                    name="password"
+                                    type="password"
+                                    id="password"
+                                    value={this.state.password}
+                                    autoComplete="current-password"
+                                    onChange={event => this.setState({ password: event.target.value })}
+
+                                />
+                            </FormControl>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="raised"
+                                color="primary"
+                                className="submit"
+                            >
+                                Sign in
+                            </Button>
+                        </form>
+                    </Paper>
+                </main>
+            </React.Fragment>
+        );
+    }
+
+}
